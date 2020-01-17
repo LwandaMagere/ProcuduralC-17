87a1f0b155521bda63d06cd30f219f80235e5de3
LwandaMagere <dadolenssen@gmail.com>
2020-01-17 19:41:00 +0300

    function implementation

--- OverloadOperator/mytime3.h
+++ OverloadOperator/mytime3.h
@@ -13,6 +13,7 @@ class Time
         Time(int h, int m = 0);
         void AddMin(int m);
         void AddHr(int h);
+        void Reset(int h = 0, int m = 0);
         Time operator+(const Time & t) const;
         Time operator-(const Time & t) const;
         Time operator*(double n)const;
