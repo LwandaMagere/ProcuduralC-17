d73cc479d8157a0aecbe4b870d217c880b7dbe24
LwandaMagere <dadolenssen@gmail.com>
2020-01-22 19:30:27 +0300

    implementation of Abstract class methods

--- AbstractClass/acctabc.h
+++ AbstractClass/acctabc.h
@@ -51,7 +51,7 @@ class BrassPlus : public AcctABC
         BrassPlus(const std::string & s = "Nullbody", long an = -1, double bal = 0.0, double ml = 500, double r = 0.10);
         BrassPlus(const Brass & ba, double ml= 500, double r = 0.1);
         virtual void ViewAcct()const;
-        virtual void withdraw(double amt);
+        virtual void Withdraw(double amt);
         void ResetMax(double m){maxLoan = m;}
         void ResetRate(double r){rate = r;};
         void ResetOwes(){owesBank = 0;}
