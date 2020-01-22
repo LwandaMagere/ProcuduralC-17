edd10e51ed05b76a363a0b0a78f54f582bd413d6
LwandaMagere <dadolenssen@gmail.com>
2020-01-22 23:47:56 +0300

    class methods

--- InheriDynamic/dma.h
+++ InheriDynamic/dma.h
@@ -38,7 +38,9 @@ class hasDMA : public baseDMA
         char * style;
     public:
         hasDMA(const char * s = "none", const char * l = "null", int r = 0);
+        hasDMA(const char * s, const baseDMA & rs);
         hasDMA(const hasDMA & hs);
+        
         ~hasDMA();
         hasDMA & operator=(const hasDMA & rs);
         friend std::ostream & operator<<(std::ostream & os, const hasDMA & rs);
