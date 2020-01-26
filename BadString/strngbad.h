90189418661a48fb0b7d2b48bdcfb101ce6406fc
LwandaMagere <dadolenssen@gmail.com>
2020-01-26 15:06:22 +0300

    flawed string class definition

--- BadString/strngbad.h
+++ BadString/strngbad.h
@@ -1,20 +1,17 @@
 // flawed string class definition
 #include <iostream>
 #ifndef STRNGBAD_H_
-#define STRNBGAD_H_
+#define STRNGBAD_H_
 class StringBad
 {
     private:
-        char * str;
-        int len;  // length of string
+        char * str; // pointer to string
+        int len; // length of string
         static int num_strings; // number of objects
     public:
-        StringBad(const char * s);
+        StringBad(const char * s); 
         StringBad();
-        ~StringBad(); // destructor
-    // friend function
-        friend std::ostream & operator <<(std::ostream & os, const StringBad & st);
-
-
-};
+        ~StringBad();
+        friend std::ostream & operator<<(std::ostream & os, const StringBad & st);
+}
 #endif
