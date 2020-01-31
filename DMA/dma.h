776bef87ce0e586b21fb8cfed57ce7f406753552
LwandaMagere <dadolenssen@gmail.com>
2020-01-31 11:48:45 +0300

    dma class methods

--- DMA/dma.h
+++ DMA/dma.h
@@ -37,6 +37,7 @@ class hasDMA : public baseDMA
     private:
         char * style;
     public:
+        hasDMA(const char * s = "none", const char * l = "null", int r = 0);
         hasDMA(const char * s, const baseDMA & rs);
         hasDMA(const hasDMA & hs);
         ~hasDMA();
