60b662a3300b1922243da94fea9ece2da0ce8d03
LwandaMagere <dadolenssen@gmail.com>
2020-02-02 18:48:28 +0300

    implicit and explicit conversion

---
+++ conversionF2/stone1.cpp
@@ -0,0 +1,15 @@
+// stone1.cpp -- user-defined conversion functions
+#include <iostream>
+#include "stonewt1.h"
+
+int main()
+{
+    using std::cout;
+    Stonewt poppins(9, 2.8);
+    double p_wt = poppins; // implicit conversion
+    cout << "Convert to double => ";
+    cout << "Poppins: " << p_wt << " pounds.\n";
+    cout << "Convert to int => ";
+    cout << "Poppins: " << int (poppins) << " pounds.\n";
+    return 0;
+}
