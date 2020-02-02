e25359e627a78c9e9b20f3b22a313125ea5ac680
LwandaMagere <dadolenssen@gmail.com>
2020-02-02 12:58:10 +0300

    implementing Time methods

--- time2/mytime0.h
+++ time2/mytime0.h
@@ -1,4 +1,7 @@
 // Time class before operator overloading
+
+/*The Time class provides methods for adjusting and resetting times, for displaying time
+values, and for adding two times.*/
 #ifndef MYTIME_H_
 #define MYTIME_H_
 
@@ -12,7 +15,7 @@ class Time
         Time(int h, int m = 0);
         void AddMin(int m);
         void AddHr(int h);
-        void Reset(int h = 0, Time m = 0);
+        void Reset(int h = 0, int m = 0);
         Time Sum(const Time & t) const;
         void Show() const;
 };
