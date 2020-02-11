00ca96fc2d93622bffa7a913363bd167f9dff4ad
LwandaMagere <dadolenssen@gmail.com>
2020-02-11 22:51:50 +0300

    methods

--- FIFO/queue.h
+++ FIFO/queue.h
@@ -31,13 +31,14 @@ class Queue
     // preemptive definition to prevent public copying
     Queue(const Queue & q) : qsize(0){}
     Queue & operator =(const Queue & q){return *this;}
+
     public:
         Queue(int qs = Q_SIZE); // create queue with a qs limit
         ~Queue();
         bool isempty() const;
         bool isfull() const;
         int queuecount() const;
-        bool enqueue(const Item &itme); // add item to end
+        bool enqueue(const Item &item); // add item to end
         bool dequeue(Item &item); // remove item from front
 };
 #endif
