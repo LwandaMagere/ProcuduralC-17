2c9ebb84ba7d80368174ef3cddc97135d6bc32d3
LwandaMagere <dadolenssen@gmail.com>
2020-02-17 17:06:23 +0300

    revisiting

--- MultileInheritance/worker0.h
+++ MultileInheritance/worker0.h
@@ -4,15 +4,15 @@
 
 #include <string>
 
-class Worker // an abstract base class
+class Worker // an abstract base class 
 {
     private:
         std::string fullname;
         long id;
     public:
-        Worker() : fullname("no  one"), id(0L){}
+        Worker() : fullname("no one"), id(0L){}
         Worker(const std::string & s, long n) : fullname(s), id(n){}
-        virtual ~Worker() = 0;
+        virtual ~Worker() = 0; // pure virtual destructor
         virtual void Set();
         virtual void Show() const;
 
@@ -24,7 +24,7 @@ class Waiter : public Worker
         int panache;
     public:
         Waiter() : Worker(), panache(0){}
-        Waiter(const std::string & s, long n, int p = 0) : Worker(s , n), panache(p){}
+        Waiter(const std::string & s, long n, int p = 0) : Worker(s, n), panache(p){}
         Waiter(const Worker & wk, int p = 0) : Worker(wk), panache(p){}
         void Set();
         void Show() const;
@@ -39,10 +39,9 @@ class Singer : public Worker
         static char *pv[Vtypes]; // string equivs of voice types
         int voice;
     public:
-        Singer(): Worker(), voice(other){}
-        Singer(const std::string & s, long n, int v = other) : Worker(s,n), voice(v){}
+        Singer() : Worker(), voice(other){}
+        Singer(const std::string & s, long n, int v = other) : Worker(s, n), voice(v){}
         void Set();
         void Show() const;
 };
-
 #endif
