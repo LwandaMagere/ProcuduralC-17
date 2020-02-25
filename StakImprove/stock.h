3918278fb495a8542634c158b08cc31d491870de
LwandaMagere <dadolenssen@gmail.com>
2020-02-25 16:01:20 +0300

    implementation

--- StakImprove/stock.h
+++ StakImprove/stock.h
@@ -1,21 +1,23 @@
-// Stock class declaration with constructors, destructor added
+// stock10.h -- Stock class declaration with constructors, destructor added
 
 #include <string>
-class Stock 
+class Stock
 {
     private:
         std::string company;
         long shares;
         double share_val;
         double total_val;
-        void set_tot(){total_val = shares * share_val;}
+        void set_tot() { total_val = shares * share_val; }
     public:
-        Stock(); // default constructor
+        // two constructors
+        Stock();
+        // default constructor
         Stock(const std::string & co, long n = 0, double pr = 0.0);
-        ~Stock(); // noisy destructor
+        ~Stock();
+        // noisy destructor
         void buy(long num, double price);
         void sell(long num, double price);
         void update(double price);
         void show();
-
 };
