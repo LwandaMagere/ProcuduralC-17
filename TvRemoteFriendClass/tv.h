aeb0db663dd3f91a0c55a840e6311da7f3532795
LwandaMagere <dadolenssen@gmail.com>
2020-03-03 15:53:18 +0300

    Remote class

--- TvRemoteFriendClass/tv.h
+++ TvRemoteFriendClass/tv.h
@@ -28,4 +28,21 @@ class Tv
         void set_input() {input = (input == TV)? DVD : TV;}
         void settings() const; // display all settings
 };
+
+class Remote 
+{
+    private:
+        int mode; // controls TV or DVD
+    public:
+        Remote(int m = Tv::TV) : mode(m){}
+        bool volup(Tv & t) { return t.volup();}
+        bool voldown(Tv & t) { return t.voldown();}
+        void onoff(Tv & t) { t.onoff(); }
+        void chanup(Tv & t) {t.chanup();}
+        void chandown(Tv & t) {t.chandown();}
+        void set_chan(Tv & t, int c) {t.channel = c;}
+        void set_mode(Tv & t) {t.set_mode();}
+        void set_input(Tv & t) {t.set_input();}
+
+};
 #endif
