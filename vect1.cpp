3120571344b51ac608e21c353f0682cabf000e4a
LwandaMagere <dadolenssen@gmail.com>
2020-03-19 11:35:29 +0300

    revisioin

--- vect1.cpp
+++ vect1.cpp
@@ -13,22 +13,22 @@ int main()
     using std::endl;
 
     vector<int> rating(NUM);
-    vector<string>titles(NUM);
-    cout << "You will do exactly as told. You will enter\n"
-         << NUM << " book titles and your ratings (0-10).\n";
-    for (int i = 0; i < NUM; i++)
+    vector<string> title(NUM);
+
+    cout << "You will do exactly as told. You will enter\n" << NUM << " book titles and your ratings (0-10).\n";
+    int i;
+    for (i = 0; i < NUM; i++)
     {
         cout << "Enter title #" << i + 1 << ": ";
-        getline(cin, titles[i]);
+        getline(cin, title[i]);
         cout << "Enter your rating (0-10): ";
         cin >> rating[i];
         cin.get();
     }
-    cout << "Thank you. You entered the following:\n"
-         << "Rating\tBook\n";
-    for (int i = 0; i < NUM; i++)
+    cout << "Thank you. You entered the following:\n" << "Rating\tBook\n";
+    for (i = 0; i < NUM; i++)
     {
-        cout << rating[i] << "\t" << titles[i] << endl;
+        cout  << rating[i] << "\t" << title[i] << endl;
     }
     return 0;
 }
