da5a7d0d1600049bcaa6ee0b1e2c165abdf0b564
LwandaMagere <dadolenssen@gmail.com>
2020-05-14 13:32:56 +0300

    reading from a file

---
+++ stringgfile.cpp
@@ -0,0 +1,32 @@
+// reading from a file
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+    ifstream fin;
+    fin.open("tobuy.txt");
+
+    if(fin.is_open() == false)
+    {
+        cerr << "Can't open file. Bye" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    string item;
+    int count = 0;
+
+    getline(fin, item, ':');
+    while (fin) // while input is good
+    {
+        ++count;
+        cout << count << ": " << item << endl;
+        getline(fin, item, ':');
+    }
+    cout << "Done" << endl;
+    fin.close();
+    return 0;
+}
