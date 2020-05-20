a5bba15f5ec8a10604674c9e3297f08578105a68
LwandaMagere <dadolenssen@gmail.com>
2020-05-20 23:45:04 +0300

    change

--- SMARTPTR.cpp
+++ SMARTPTR.cpp
@@ -16,16 +16,16 @@ class Report
 
 int main()
 {
-    {
+   /* {
         std::auto_ptr<Report> ps (new Report("using auto_ptr"));
         ps->comment();
-    }
+    }*/
 
     {
         std::shared_ptr<Report> ps (new Report("using shared_ptr"));
         ps->comment();
     }
-    
+
     {
         std::unique_ptr<Report> ps (new Report("using unique_ptr"));
         ps->comment();
