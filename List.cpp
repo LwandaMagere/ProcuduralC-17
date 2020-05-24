9082df8c04374099252e4a5a3f5c5874a71e54c9
LwandaMagere <dadolenssen@gmail.com>
2020-05-24 21:09:37 +0300

    testing

--- List.cpp
+++ List.cpp
@@ -24,6 +24,28 @@ int main()
     for_each(two.begin(), two.end(), outint);
     cout << endl << "List three: ";
     for_each(three.begin(), three.end(), outint);
-    
+    cout << endl;
+    three.remove(2);
+    cout << endl << "List three minus 2s: ";
+    for_each(three.begin(), three.end(), outint);
+    three.splice(three.begin(), one);
+    cout << endl << "List three after splice: ";
+    for_each(three.begin(), three.end(), outint);
+
+    for_each(three.begin(), three.end(), outint);
+    cout << endl << "List one: ";
+    for_each(one.begin(), one.end(), outint);
+    three.unique();
+    cout << endl << "List three after unique: ";
+    for_each(three.begin(), three.end(), outint);
+    three.sort();
+    three.unique();
+    cout << endl << "List three after sort & unique: ";
+    for_each(three.begin(), three.end(), outint);
+    two.sort();
+    three.merge(two);
+    cout << endl << "Sorted two merged into three: ";
+    for_each(three.begin(), three.end(), outint);
+    cout << endl;
     return 0;
 }
