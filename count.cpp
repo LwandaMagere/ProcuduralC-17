81da41d54766354d6c62349a90ca4e767c4213de
LwandaMagere <dadolenssen@gmail.com>
2020-06-24 21:20:25 +0300

    modified

--- count.cpp
+++ count.cpp
@@ -7,33 +7,35 @@ int main(int argc, char * argv[])
 {
     using namespace std;
 
-    if (argc == 1)
+    if (argc == 1) // quit if no arguments
     {
-        cerr << "Usage: " << argv[0] << " filename[s]\n";
+        cerr << "Usage: " << argv[0] << "filename[s]";
         exit(EXIT_FAILURE);
     }
 
-    ifstream fin;
+    ifstream fin; // open stream
     long count;
     long total = 0;
     char ch;
 
     for (int file = 1; file < argc; file++)
     {
-       fin.open(argv[file]); // connect stream to argv[file]
-       if (!fin.is_open())
-       {
-           cerr << "Could not open " << argv[file] << endl;
-           fin.clear();
-           continue;
-       }
-       count = 0;
-       while (fin.get(ch))
+        fin.open(argv[file]); // connect stream to argv[file]
+        if(!fin.is_open())
+        {
+            cerr << "Could not open " << argv[file] << endl;
+            fin.clear();
+            continue;
+        }
+        count = 0;
+        while (fin.get(ch))
             count++;
-       cout << count << " characters in " << argv[file] << endl;
-       total += count;
-       fin.clear(); // needed for some implementations
-       fin.close();
+        cout << count << " characters in " << argv[file] << endl;
+        total = total + count;
+        fin.clear();
+        fin.close();
+        cout << total << " characters in all files\n";
     }
-    cout << total << " characters in all files\n";
+
+    return 0;
 }
