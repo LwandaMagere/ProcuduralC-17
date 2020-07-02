56732258d7405532d7f01f85114a6a303cd25d68
LwandaMagere <dadolenssen@gmail.com>
2020-07-02 22:11:17 +0300

    multiple files

---
+++ coodinate2/file2.cpp
@@ -0,0 +1,23 @@
+// contains functions called in file1.cpp
+#include <iostream>
+#include <cmath>
+#include "coordin.h" // structure templates, function prototypes
+
+polar rect_to_polar(rect xypos)
+{
+    using namespace std;
+
+    polar answer;
+    sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+    answer.angle = atan2(xypos.y, xypos.x);
+    return answer; // return a polar structure
+}
+
+void show_polar(polar dapos)
+{
+    using namespace std;
+    const double Rad_to_deg = 57.29577951;
+    cout << "distance = " << dapos.distance;
+    cout << ", angle = " << dapos.angle * Rad_to_deg;
+    cout << " degrees\n";
+}
