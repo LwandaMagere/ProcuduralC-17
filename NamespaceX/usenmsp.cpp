d05531c4319b9f606959fa8bdaf9793fd0fc1f1d
LwandaMagere <dadolenssen@gmail.com>
2020-07-05 00:14:43 +0300

    namespaces

---
+++ NamespaceX/usenmsp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "namesp.h"
+
+void other(void);
+void another(void);
+
+int main(void)
+{
+    using debts::Debt;
+    
+    using debts::showDebt;
+    Debt golf = { {"Benny", "Goatsniff"}, 120.0 };
+    showDebt(golf);
+    other();
+    another();
+    return 0;
+}
