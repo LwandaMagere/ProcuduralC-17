4255ba83c7f5546f9fcc24d5ae823cd4ab2a7490
LwandaMagere <dadolenssen@gmail.com>
2020-07-09 09:34:47 +0300

    raw string

---
+++ RawString.cpp
@@ -0,0 +1,11 @@
+// using a raw string
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using namespace std;
+
+    cout << R"("This is a raw string bana! "\n")" << endl;
+    return 0;
+}
