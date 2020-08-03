fd0c652f2a41470ac9da915d2c8d1cb0f9919823
LwandaMagere <dadolenssen@gmail.com>
2020-08-03 23:19:23 +0300

    recursion

---
+++ recur1.cpp
@@ -0,0 +1,19 @@
+// using recursion
+#include <iostream>
+
+void countdown(int n);
+
+int main()
+{
+    countdown(4); // call the recursive function
+    return 0;
+}
+
+void countdown(int n)
+{
+    using namespace std;
+    cout << "Counting down ... " << n << endl;
+    if (n > 0)
+        countdown(n - 1);
+    cout << n << ": Kaboom!\n";
+}
