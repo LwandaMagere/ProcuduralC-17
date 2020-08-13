fa46970648c1beb67e6f233fdcb0708852d2be96
LwandaMagere <dadolenssen@gmail.com>
2020-08-13 10:28:21 +0300

    revision

---
+++ Inheritance1/tabtenn0.h
@@ -0,0 +1,21 @@
+// a table tennis base class
+#ifndef TABTENNIS_H_
+#define TABTENNIS_H_
+#include <string>
+using std::string;
+// simple base class
+class TabTennisPlayer
+{
+    private:
+        string firstname;
+        string lastname;
+        bool hasTable;
+    public:
+        TabTennisPlayer(const string & fn = "none", const string & ln = "none", bool ht = false);
+        void Name() const;
+        bool HasTable()const {return hasTable;}
+        void ResetTable(bool v){ hasTable = v;}
+
+};
+
+#endif
