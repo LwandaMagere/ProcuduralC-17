4201a94953691390c09d0e3dcd582dcdf0594d51
LwandaMagere <dadolenssen@gmail.com>
2020-08-16 21:53:28 +0300

    modified

--- Inheritance1/dma.h
+++ Inheritance1/dma.h
@@ -31,15 +31,18 @@ public:
 };
 
 // derived class with DMA
-class hasDMA : public baseDMA
+class hasDMA :public baseDMA
 {
 private:
     char * style;
 public:
-    hasDMA(const char * s = "none", const char * l = "null", int r = 0);
-    hasDMA(const char * s, const baseDMA & rs);
-    hasDMA(const hasDMA & hs);
-    ~hasDMA();
-    hasDMA & operator=(const hasDMA & rs);
-    friend std::ostream operator<<(std::ostream & os, const hasDMA & rs);
+hasDMA(const char * s = "none", const char * l = "null",
+int r = 0);
+hasDMA(const char * s, const baseDMA & rs);
+hasDMA(const hasDMA & hs);
+~hasDMA();
+hasDMA & operator=(const hasDMA & rs);
+friend std::ostream & operator<<(std::ostream & os,
+const hasDMA & rs);
 };
+#endif
