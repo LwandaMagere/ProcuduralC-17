3e2abb24005ce06945b71ded94062c1b8c088426
LwandaMagere <dadolenssen@gmail.com>
2020-09-18 17:32:39 +0300

    irevision

--- StockRevision/studentd.h
+++ StockRevision/studentd.h
@@ -23,6 +23,8 @@ class Student
         ~Student(){}
         double Average() const;
         const std::string & Name()const;
+        double & operator[](int i);
+        double operator[](int i) const;
         friend std::istream & operator>>(std::istream & is, Student & stu);
         friend std::istream & getline(std::istream & is, Student & stu);
         friend std::ostream & operator<<(std::ostream & os, const Student & stu);
