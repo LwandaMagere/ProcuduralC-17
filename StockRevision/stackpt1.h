376f4cef3102b6581603263264f8e4e4ebbe9d8f
LwandaMagere <dadolenssen@gmail.com>
2020-09-20 11:35:20 +0300

    modified

--- StockRevision/stackpt1.h
+++ StockRevision/stackpt1.h
@@ -20,4 +20,57 @@ class Stack
         Stack & operator=(const Stack & st);
 };
 
+template <class T>
+Stack<T>::Stack(int ss) : stacksize(ss), top(0)
+{
+    items = new T [stacksize];
+}
+
+template <class T>
+Stack<T>::Stack(const Stack & st)
+{
+    stacksize = st.stacksize;
+    top = st.top;
+    items = new T [stacksize];
+    for (int i  = 0 ; i < top; i++)
+        items[i] = st.items[i];
+}
+
+template <class T>
+bool Stack<T>::push(const T & item)
+{
+    if (top < stacksize)
+    {
+        items[top++] = item;
+        return true;
+    }
+    else 
+        return false;
+}
+
+template <class T>
+bool Stack<T>::pop(T & item)
+{
+    if (top > 0)
+    {
+        item = items[--top];
+        return true;
+    }
+    else 
+        return false;
+}
+
+template <class T>
+Stack<T> & Stack<T>::operator=(const Stack<T> & st)
+{
+    if (this == &st)
+        return *this;
+    delete [] items;
+    stacksize = st.stacksize;
+    top = st.top;
+    items = new T [stacksize];
+    for (int i = 0; i < top; i++)
+        items[i] = st.items[i];
+    return *this;
+}
 #endif
