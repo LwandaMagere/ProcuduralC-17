b6626c5c67758fbb492c2604ecb6869babb64a60
LwandaMagere <dadolenssen@gmail.com>
2020-09-25 22:09:19 +0300

    revi

---
+++ StockRevision/smartptr.cpp
@@ -0,0 +1,31 @@
+// using three kinds of smart pointers
+#include <iostream>
+#include <string>
+#include <memory>
+
+class Report
+{
+    private:
+        std::string str;
+    public:
+        Report(const std::string s) : str(s)
+        {std::cout << "Object created!\n";}
+        ~Report(){std::cout << "Object deleted!\n";}
+        void comment() const {std::cout << str << "\n";}
+};
+
+int main()
+{
+    {
+        std::auto_ptr<Report> ps (new Report("using auto_ptr"));
+        ps->comment(); // use-> to invoke a member function
+    }
+    {
+        std::shared_ptr<Report> ps (new Report("using shared_ptr"));
+        ps->comment();
+    }
+    {
+        std::unique_prt<Report> ps (new Report("using unique_prt"));
+        ps->comment();
+    }
+}
