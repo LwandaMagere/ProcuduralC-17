5a7dc545617d80018c620cb02c8a4711af2307c4
LwandaMagere <dadolenssen@gmail.com>
2020-10-19 17:32:09 +0300

    r

--- objects.cpp
+++ objects.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
 int main()
 {
+     int top = 0;
+    char  alpha[5] = {'a','b','c','d','e'};
+    char item = 'D';
+
+    alpha[++top] = item;
+
+    //for (int i = 0; i <= 0; i++)
+        cout << alpha[top] << " 1 " << top << endl;
+        cout << alpha[0] << " 0 " << top << endl;
+        cout << alpha[2] << " 2 " << top << endl;
+        cout << alpha[3] << " 3 " << top << endl;
+        cout << alpha[4] << " 4 " << top << endl;
+
     return 0;
 }
