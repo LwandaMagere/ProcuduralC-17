1ca482b8970ca99a637b02645645d606fb325b64
LwandaMagere <dadolenssen@gmail.com>
2020-10-23 15:42:09 +0300

    r

---
+++ AbstractBank/Stock2.h
@@ -0,0 +1,22 @@
+// Stock class interface
+#ifndef STOCK2_H_
+#define STOCK2_H_
+#include <string>
+#include <iostream>
+
+class Stock
+{
+    private:
+        std::string company;
+        long shares;
+        double share_val;
+        double total_val;
+        void set_tot(){total_val =  shares * share_val; }
+    public:
+        void acquire(const std::string & co, long n, double pr);
+        void buy(long num, double price);
+        void update(double price);
+        void show();
+};
+
+#endif
