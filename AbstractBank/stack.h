42b3386ca26b28a4679129fe5fc711a86b0fa0d7
LwandaMagere <dadolenssen@gmail.com>
2020-10-23 22:59:53 +0300

    r

---
+++ AbstractBank/stack.h
@@ -0,0 +1,24 @@
+// class definition for the stack ADT
+#ifndef STACK1
+#define STACK1
+
+typedef unsigned long Item;
+
+class Stack
+{
+    private:
+        enum {MAX = 10};
+        Item items[MAX]; // holds stack items
+        int top; // index for top stack item
+    public:
+        Stack();
+        bool isempty() const;
+        bool isfull() const;
+        // push() returns false if stack already is full, true otherwise
+        bool push(const Item & item); // add item to stack
+        // pop() returns false if stack already is empty, true otherwise
+        bool pop(Item & item);
+
+};
+
+#endif
