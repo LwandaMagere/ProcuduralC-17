1132cb77e1ee95175579e40df81a6b617c16ea02
LwandaMagere <dadolenssen@gmail.com>
2020-10-25 09:31:19 +0300

    r

---
+++ AbstractBank/stonewt.h
@@ -0,0 +1,21 @@
+// definition for the Stonewt class
+#ifndef STONEWT
+#define STONEWT
+
+class Stonewt
+{
+    private:
+        enum {Lbs_per_stn = 14};
+        int stone;
+        double pds_left;
+        double pounds;
+    public:
+        Stonewt(double lbs);
+        Stonewt(int stn, double lbs);
+        Stonewt();
+        ~Stonewt();
+        void show_lbs() const;
+        void show_stn() const;
+};
+
+#endif
